5408aa48d445a48884d9213f416f10d27ec7c84d
LOKESH KUMAR NATHI <147913462+lokeshkumarnathi@users.noreply.github.com>
2026-09-22 10:40:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((input, index) => {
+  input.addEventListener("input", function () {
+    // Allow only digits
+    input.value = input.value.replace(/\D/g, "");
+
+    if (input.value !== "" && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", function (event) {
+    if (event.key === "Backspace") {
+      if (input.value === "" && index > 0) {
+        codes[index - 1].focus();
+        codes[index - 1].value = "";
+      } else {
+        input.value = "";
+      }
+    }
+  });
+});
